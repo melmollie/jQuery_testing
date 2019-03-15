3c1ec576fb46c6a9a3ae2336c7a1f3293b1bb3d2
Elli Vähäkangas <elli.vahakangas@gmail.com>
2019-03-15 15:31:03 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+$("#hello").append(" <span>...Nope.</span>");
+$("img").attr("src", "cool1.png");
+$("img").removeClass("tableimg");
+$("img").addClass("cool");
+$(".cool").css({"width": "200px"});
+
+$("#demo").html("<b>Lol</b> what's going on in here <em>huh??</em>");
+$("#demo2").text("<b>Lol</b> what's going on in here <em>huh??</em>");
+$("#demo2").addClass("moi");
+$("#demo2").before("<h4 class='title'>This is how it looks like in editor:</h4>");
+
+$(function(){
+    let divi = $("<div class='new-div'></div>").html("This is a new div created with jQuery");
+    $("#demo2").after(divi); 
+});
+
+$(function() {
+    let footer = $("<footer></footer>").html("This is footer.");
+    $(".content-wrapper").after(footer);
+});
+
+$(function() {
+    $("#color-button").click(function() {
+        $(".new-div").toggleClass("bgcolor");
+    });
+});
+
+$(function() {
+    $("#delete-button").click(function() {
+        $("body").empty();
+        alert("You fool!");
+    });
+});
+
+$(function() {
+    $("#demo").click(function() {
+        $("#demo").text("<b>Lol</b> what's going on in here <em>huh??</em>");
+        $("#demo").click(function(){
+            $("#demo").html("<b>Lol</b> what's going on in here <em>huh??</em>");
+        });
+    });
+});
+
+$(function() {
+    $("#add").on("click", function() {
+        let val = $("input").val();
+        if(val !== '') {
+            let elem = $("<li></li>").text(val);
+            $(elem).append("<button class='rem'>X</button>");
+            $("#mylist").append(elem);
+            $("input").val("");
+            $(".rem").on("click", function() {
+                $(this).parent().remove();
+            });
+        }
+    });
+});
